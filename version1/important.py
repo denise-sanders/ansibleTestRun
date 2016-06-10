c788428a4ede33973e679184842f2577b8957156
Denise Irvin <deni0109@mq1372dv30.rackspace.corp>
2016-06-10 12:58:27 -0500

    gave old version:

---
+++ version1/important.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/index')
+@app.route('/')
+def hello_world():
+    return 'I\'m deployed! Thanks x 1000!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
